6fb2fc281ae994c963636c73f8a75a8718f4c21b
gal42 <t-carlag@microsoft.com>
2022-08-01 10:18:03 -0700

    Implemented LogOut feature

--- AzureADSample/AzureADSample/ViewModel/ProfilePageViewModel.cs
+++ AzureADSample/AzureADSample/ViewModel/ProfilePageViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using AzureADSample.Model;
+using AzureADSample.Service;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Identity.Client;
 
 namespace AzureADSample.ViewModel;
 
@@ -8,4 +10,16 @@ public partial class ProfilePageViewModel : ObservableObject
 {
     [ObservableProperty]
     User user;
+    async void LogOut(object sender, EventArgs e)
+    {
+        IEnumerable<IAccount> accounts = await AuthService.authenticationClient.GetAccountsAsync();
+
+        while (accounts.Any())
+        {
+            await AuthService.authenticationClient.RemoveAsync(accounts.First());
+            accounts = await AuthService.authenticationClient.GetAccountsAsync();
+        }
+
+        await Shell.Current.GoToAsync(nameof(MainPage));
+    }
 }
