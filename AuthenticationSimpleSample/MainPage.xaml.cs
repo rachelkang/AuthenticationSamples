3256760feef64bfd0086786aba0f3281c0071871
Leandra Hall <90208577+LeandraHall@users.noreply.github.com>
2022-08-09 11:40:24 -0700

    The most recent version of the Authentication Simple Sample (#19)
    * The most recent version of the Authentication Simple Sample for Azure AD B2C
    
    * Updates to .csproj file to show changes in provisioning profile
    
    * The most updated version of the AuthenticationSimpleSample that functions on Android, Windows, and iOS
    
    Co-authored-by: Admin <admin@Admins-MacBook-Pro.local>
    Co-authored-by: Rachel Kang <rachelkang@microsoft.com>

---
+++ AuthenticationSimpleSample/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using AuthenticationSimpleSample.Auth;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace AuthenticationSimpleSample;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+        InitializeComponent();
+
+        // Make sure that  resultCode == SecStatusCode.Success
+    }
+
+    async void AzureADPageClicked(object sender, EventArgs args)
+    {
+        Console.WriteLine("The breakpoint should be hitting rn");
+        var authService = new AuthService(); // most likely you will inject it in constructor, but for simplicity let's initialize it here
+        Console.WriteLine("Hello");
+        var webResult = await authService.LoginAsync(CancellationToken.None);
+        Console.WriteLine("It's me");
+        var token = webResult?.IdToken; // you can also get AccessToken if you need it
+        Console.WriteLine("Wow!");
+        if (token != null)
+        {
+            Console.WriteLine("This is the second breakpoint");
+            var handler = new JwtSecurityTokenHandler();
+            var data = handler.ReadJwtToken(token);
+            var claims = data.Claims.ToList();
+            if (data != null)
+            {
+                Console.WriteLine("omg pls work");
+                await Shell.Current.GoToAsync(nameof(SettingsPage));
+                var stringBuilder = new StringBuilder();
+                stringBuilder.AppendLine($"Name: {data.Claims.FirstOrDefault(x => x.Type.Equals("name"))?.Value}");
+                stringBuilder.AppendLine($"Email: {data.Claims.FirstOrDefault(x => x.Type.Equals("preferred_username"))?.Value}");
+                LoginResultLabel.Text = stringBuilder.ToString();
+            }
+        }
+    }
+
+
+}
+
---
+++ AuthenticationSimpleSample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,17 @@
+﻿using Foundation;
+using Microsoft.Identity.Client;
+using UIKit;
+using AuthenticationServices;
+
+namespace AuthenticationSimpleSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+    {
+        AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(url);
+        return base.OpenUrl(app, url, options);
+    }
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
