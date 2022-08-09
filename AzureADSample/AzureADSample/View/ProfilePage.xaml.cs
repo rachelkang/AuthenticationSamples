d9352f5229b18630249ff84c74dec880053172a5
gal42 <t-carlag@microsoft.com>
2022-08-09 11:11:11 -0700

    Final LogOut implementation
    Added LogOut feature that calls from the ViewModel instead of the back code

--- AzureADSample/AzureADSample/View/ProfilePage.xaml.cs
+++ AzureADSample/AzureADSample/View/ProfilePage.xaml.cs
@@ -6,23 +6,9 @@ namespace AzureADSample.Views;
 
 public partial class ProfilePage : ContentPage
 {
-	public ProfilePage(ProfilePageViewModel vm)
+	public ProfilePage()
 	{
 		InitializeComponent();
-		BindingContext = vm;
+		BindingContext = new ProfilePageViewModel();
 	}
-
-    async void LogOutBtn(object sender, EventArgs e)
-    {
-        IEnumerable<IAccount> accounts = await AuthService.authenticationClient.GetAccountsAsync();
-
-        while (accounts.Any())
-        {
-            await AuthService.authenticationClient.RemoveAsync(accounts.First());
-            accounts = await AuthService.authenticationClient.GetAccountsAsync();
-        }
-        //Redirect user to home page
-        await Shell.Current.GoToAsync(nameof(MainPage));
-    }
 }
-
